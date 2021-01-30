c32787273ff3aab2ddac35394fffaab96d48ea82
Samsung T5 - Program <Samsung T5 - Program>
2021-01-30 14:38:38 +0800

    fix

--- src/EM-CMS/replace/vue.config.js
+++ src/EM-CMS/replace/vue.config.js
@@ -24,9 +24,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/admin/',
+  publicPath: '/',
   outputDir: 'dist',
-  assetsDir: '/em/static',
+  assetsDir: 'em/static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
